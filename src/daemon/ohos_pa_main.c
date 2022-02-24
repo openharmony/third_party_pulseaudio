0912cfdf9dd12258b115efc6963d968d31216cba
Sulav Mulmi <sulav.mulmi@huawei.com>
2022-02-24 14:32:38 +0530

    Remove read and write permission for cookie file for other users
    Signed-off-by: Sulav Mulmi <sulav.mulmi@huawei.com>

--- src/daemon/ohos_pa_main.c
+++ src/daemon/ohos_pa_main.c
@@ -1199,7 +1199,7 @@ int ohos_pa_main(int argc, char *argv[]) {
     change_permission("/data/data/.pulse_dir", 0755);
     change_permission("/data/data/.pulse_dir/runtime", 0755);
     change_permission("/data/data/.pulse_dir/state", 0755);
-    change_permission("/data/data/.pulse_dir/state/cookie", 0666);
+    change_permission("/data/data/.pulse_dir/state/cookie", 0660);
 
     pa_log_info("Daemon startup complete.");
 
