20ad94d3a6f1abc06820851d06e39149caf8e3aa
AOL <lixin113@huawei.com>
2024-02-07 01:35:51 +0000

    fixed 785646f from https://gitee.com/magekkkk/third_party_pulseaudio/pulls/78 modify auto resampler
    Signed-off-by: AOL <lixin113@huawei.com>

--- src/pulsecore/resampler.c
+++ src/pulsecore/resampler.c
@@ -36,6 +36,8 @@
 /* Number of samples of extra space we allow the resamplers to return */
 #define EXTRA_FRAMES 128
 
+#define AUDIO_HIGH_QUALITY_RATE 40000
+
 struct ffmpeg_data { /* data specific to ffmpeg */
     struct AVResampleContext *state;
 };
@@ -140,15 +142,16 @@ static void calculate_gcd(pa_resampler *r) {
     r->gcd = gcd;
 }
 
-static pa_resample_method_t choose_auto_resampler(pa_resample_flags_t flags) {
+static pa_resample_method_t choose_auto_resampler(pa_resample_flags_t flags,
+    const uint32_t rate_a, const uint32_t rate_b) {
     pa_resample_method_t method;
 
-    if (pa_resample_method_supported(PA_RESAMPLER_SPEEX_FLOAT_BASE + 1))
+    if (pa_resample_method_supported(PA_RESAMPLER_SPEEX_FLOAT_BASE + 1) &&
+        (rate_a > AUDIO_HIGH_QUALITY_RATE) && (rate_a != rate_b)) {
         method = PA_RESAMPLER_SPEEX_FLOAT_BASE + 1;
-    else if (flags & PA_RESAMPLER_VARIABLE_RATE)
+    } else {
         method = PA_RESAMPLER_TRIVIAL;
-    else
-        method = PA_RESAMPLER_FFMPEG;
+    }
 
     return method;
 }
@@ -203,7 +206,7 @@ static pa_resample_method_t fix_method(
     }
 
     if (method == PA_RESAMPLER_AUTO)
-        method = choose_auto_resampler(flags);
+        method = choose_auto_resampler(flags, rate_a, rate_b);
 
 #ifdef HAVE_SPEEX
     /* At this point, method is supported in the sense that it
