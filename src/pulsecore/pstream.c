c70b03c5574fde421f916f06d8c078a278be005d
guoyao <guoyao18@huawei.com>
2024-08-24 18:11:56 +0800

    pulseaudio 新增日志和trace
    Signed-off-by: guoyao <guoyao18@huawei.com>

--- src/pulsecore/pstream.c
+++ src/pulsecore/pstream.c
@@ -42,6 +42,7 @@
 #include <pulsecore/macro.h>
 
 #include "pstream.h"
+#include "log/audio_log.h"
 
 /* We piggyback information if audio data blocks are stored in SHM on the seek mode */
 #define PA_FLAG_SHMDATA     0x80000000LU
@@ -471,6 +472,9 @@ void pa_pstream_send_packet(pa_pstream*p, pa_packet *packet, pa_cmsg_ancil_data 
 #endif
 
     pa_queue_push(p->send_queue, i);
+    if (PaQueueGetLen(p->send_queue) >= 3) {  // 3 maybe have msg backlog
+        AUDIO_WARNING_LOG("[MSG backlog]: @<%{public}p> PaQueueLen = %{public}u", p, PaQueueGetLen(p->send_queue));
+    }
 
     p->mainloop->defer_enable(p->defer_event, 1);
 }
