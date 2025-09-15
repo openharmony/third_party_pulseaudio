ee4825287be5060e980adc184d47e5856879676f
蒋志庆 <jiangzhiqing4@h-partners.com>
2025-09-15 16:19:09 +0800

    pulseAudio 日志去除
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- src/pulsecore/asyncmsgq.c
+++ src/pulsecore/asyncmsgq.c
@@ -157,7 +157,6 @@ int pa_asyncmsgq_send(pa_asyncmsgq *a, pa_msgobject *object, int code, const voi
     struct asyncmsgq_item i;
     pa_assert(PA_REFCNT_VALUE(a) > 0);
 
-    pa_usec_t startTime = pa_rtclock_now();
     char t[PA_SNPRINTF_STR_LENGTH] = {0};
     pa_snprintf(t, sizeof(t), "pa_asyncmsgq_send[%d] <msgqNo.%u> msg_wait_for_read:%u", code, a->mark,
         PaAsyncqGetNumToRead(a->asyncq));
@@ -187,12 +186,6 @@ int pa_asyncmsgq_send(pa_asyncmsgq *a, pa_msgobject *object, int code, const voi
     if (pa_flist_push(PA_STATIC_FLIST_GET(semaphores), i.semaphore) < 0)
         pa_semaphore_free(i.semaphore);
     CallEnd();
-    pa_usec_t executionTime = pa_rtclock_now() - startTime;
-    if (executionTime > OH_DAEMON_TIMEOUT_THRESHOLD_ON_US) { // too long block of daemon thread, dangerous
-        AUDIO_WARNING_LOG("Execution time of this msg is too long: qLen[%{public}u], MSG[%{public}d] " \
-            "(%{public}" PRIu64 "ms)",
-            PaAsyncqGetNumToRead(a->asyncq), code, executionTime / PA_USEC_PER_MSEC);
-    }
     return i.ret;
 }
 
